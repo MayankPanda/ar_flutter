e061e141d9ffbb299e31b58842edaa9b6019f1c8
MayankPanda <mayankpanda7305@gmail.com>
2022-06-09 15:38:59 +0530

    added files

---
+++ flutter_ar/lib/main.dart
@@ -0,0 +1,79 @@
+import 'dart:io';
+
+import 'package:ar_flutter_plugin/managers/ar_location_manager.dart';
+import 'package:ar_flutter_plugin/managers/ar_session_manager.dart';
+import 'package:ar_flutter_plugin/managers/ar_object_manager.dart';
+import 'package:ar_flutter_plugin/managers/ar_anchor_manager.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
+import 'package:ar_flutter_plugin/datatypes/config_planedetection.dart';
+import 'package:ar_flutter_plugin/datatypes/node_types.dart';
+import 'package:ar_flutter_plugin/models/ar_node.dart';
+import 'package:flutter/services.dart';
+import 'package:vector_math/vector_math_64.dart';
+import 'dart:math';
+
+class LocalAndWebObjectsWidget extends StatefulWidget {
+  LocalAndWebObjectsWidget({Key? key}) : super(key: key);
+  @override
+  _LocalAndWebObjectsWidgetState createState() =>
+      _LocalAndWebObjectsWidgetState();
+}
+
+class _LocalAndWebObjectsWidgetState extends State<LocalAndWebObjectsWidget> {
+  ARSessionManager? arSessionManager;
+  ARObjectManager? arObjectManager;
+  //String localObjectReference;
+  ARNode? localObjectNode;
+  //String webObjectReference;
+
+  @override
+  void dispose() {
+    super.dispose();
+    arSessionManager!.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Local & Web Objects'),
+      ),
+      body: ARView(
+        onARViewCreated: onARViewCreated,
+        planeDetectionConfig: PlaneDetectionConfig.horizontalAndVertical,
+      ),
+    );
+  }
+
+  void onARViewCreated(
+      ARSessionManager arSessionManager,
+      ARObjectManager arObjectManager,
+      ARAnchorManager arAnchorManager,
+      ARLocationManager arLocationManager) {
+    this.arSessionManager = arSessionManager;
+    this.arObjectManager = arObjectManager;
+
+    this.arSessionManager!.onInitialize(
+          showFeaturePoints: false,
+          showPlanes: true,
+          customPlaneTexturePath: "images/360.jfif",
+          showWorldOrigin: true,
+          handleTaps: false,
+        );
+    this.arObjectManager!.onInitialize();
+    var newNode = ARNode(
+        type: NodeType.localGLTF2,
+        uri: "images/asset.gltf",
+        scale: Vector3(0.2, 0.2, 0.2),
+        position: Vector3(0.0, 0.0, 0.0),
+        rotation: Vector4(1.0, 0.0, 0.0, 0.0));
+    arObjectManager.addNode(newNode);
+    //Download model to file system
+    // Alternative to use type fileSystemAppFolderGLTF2:
+    //_downloadAndUnpack(
+    //    "https://drive.google.com/uc?export=download&id=1fng7yiK0DIR0uem7XkV2nlPSGH9PysUs",
+    //    "Chicken_01.zip");
+  }
+}
